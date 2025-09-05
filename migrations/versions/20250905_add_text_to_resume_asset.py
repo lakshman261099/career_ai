4adc258e74312eb137956f393e1d8d600a5df588
lakshman261099 <lakshman.261099@gmail..com>
2025-09-05 22:08:19 +0100

    added table

---
+++ migrations/versions/20250905_add_text_to_resume_asset.py
@@ -0,0 +1,22 @@
+"""add text column to resume_asset
+
+Revision ID: 20250905_add_text_to_resume_asset
+Revises: 
+Create Date: 2025-09-05 21:40:00.000000
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "20250905_add_text_to_resume_asset"
+down_revision = "<timestamp>"
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    with op.batch_alter_table("resume_asset") as batch_op:
+        batch_op.add_column(sa.Column("text", sa.Text(), nullable=True))
+
+def downgrade():
+    with op.batch_alter_table("resume_asset") as batch_op:
+        batch_op.drop_column("text")
